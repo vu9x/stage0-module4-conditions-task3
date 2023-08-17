9dd0adbc052e0fd647c98f3d31b6adedb0e469ae
Vu <nguyentuanvu9x@gmail.com>
2023-08-17 21:52:43 +0300

    Adding Seasons.java

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,24 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
+        if (month > 12 || month <= 0) {
+            System.out.println("wrong number!");
+        } else {
+            if (month >= 3 && month <= 5) {
+                System.out.println("Spring");
+            }
 
+            if (month >= 6 && month <= 8) {
+                System.out.println("Summer");
+            }
+
+            if (month >= 9 && month <= 11) {
+                System.out.println("Autumn");
+            }
+
+            if (month <= 2 || month == 12) {
+                System.out.println("Winter");
+            }
+        }
     }
 }
