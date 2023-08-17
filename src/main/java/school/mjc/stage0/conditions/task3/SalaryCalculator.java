94a8e819f722455e18b1be03231e15b83dd7c13a
Vu <nguyentuanvu9x@gmail.com>
2023-08-17 22:14:10 +0300

    Adding: SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0) {
+            System.out.println("wrong input");
+        } else {
+            if (salary <= 10_000) {
+                System.out.println(salary  - salary * 0.15);
+            }
+            if (salary <= 20_000 && salary > 10_000) {
+                System.out.println(salary  - salary * 0.18);
+            }
+            if (salary > 20_000) {
+                System.out.println(salary  - salary * 0.20);
+            }
+        }
     }
 }
