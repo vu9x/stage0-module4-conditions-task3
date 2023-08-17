06b61f7fdb8a17bb7b37d168ae28fc0ac11d5e08
Vu <nguyentuanvu9x@gmail.com>
2023-08-17 22:06:12 +0300

    Adding: isEnglishSymbolDeterminter.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        int intSymbol = symbol;
+        int a = 'a';
+        int z = 'z';
+        int A = 'A';
+        int Z = 'Z';
+
+        if (intSymbol > A && intSymbol < Z || intSymbol > a && intSymbol < Z) {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
